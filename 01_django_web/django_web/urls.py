868758d08dad1a04a9c5a9e797417693a4d52aa5
JUYEOP024 <juyeobsong63@gmail.com>
2026-03-20 15:30:51 +0900

    클라우드 2일차

--- 01_django_web/django_web/urls.py
+++ 01_django_web/django_web/urls.py
@@ -16,8 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from app.views import home 
-
+from app.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
--- 01_django_web/static/app/script.js
+++ 01_django_web/static/app/script.js
@@ -1,3 +1,3 @@
 function showAlert() {
-    alert("Django 웹 서버 정상적으로 실행되고 있습니다!");
+    alert('Django 웹 서버 정상 작동 중입니다!');
 }
